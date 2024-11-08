9bb37a11b2a6cab2b6c6ea9c51d4064977df2195
Pablo <Pablo@DESKTOP-G7LCRNL>
2024-11-08 12:07:29 +0100

    Modificado el Resultado del Ejercicio 1

--- EjerciciosClaseJava/src/Ejercicios/Ejercicio1.java
+++ EjerciciosClaseJava/src/Ejercicios/Ejercicio1.java
@@ -5,7 +5,7 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		
 		int hora = 3;
-		int minutos = 30;
+		int minutos = 59;
 		int segundos = 59;
 		
 		int horaResultado = hora;
@@ -14,8 +14,10 @@ public class Ejercicio1 {
 		
 		if (segundoResultado == 60) {
 			minutoResultado++;
+			segundoResultado = 0;
 			if (minutoResultado ==60) {
 				horaResultado++;
+				minutoResultado = 0;
 				if (horaResultado == 24) {
 					horaResultado = 0;
 				}
